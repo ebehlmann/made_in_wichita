aa237dd7713224c40fcbd881253da1daac6d3a75
Emily Behlmann <ebehlmann@gmail.com>
2014-11-02 20:34:34 -0600

    Added create, edit and delete functions for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,13 @@ class ProductsController < ApplicationController
 	end
 
 	def create
+		@product = Product.new(params[:product])
+		if @product.save
+			flash[:notice] = "Product created."
+			redirect_to @products_path
+		else
+			render 'new'
+		end
 	end
 
 	def show
@@ -15,11 +22,23 @@ class ProductsController < ApplicationController
 	end
 
 	def edit
+		@product = Product.find(params[:id])
 	end
 
 	def update
+		@product = Product.new(params[:product])
+		if @product.update
+			flash[:notice] = "Product updated."
+			redirect_to product_path(@product)
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
+		@product = Product.find(params[:id])
+		@product.destroy
+		flash[:notice] = "Product deleted."
+		redirect_to @products_path
 	end
 end
